3ecc4c43f56f646caa16029a535a8cd431f6de7d
Premkumar Masilamani <prem@smileprem.com>
2022-08-02 22:12:36 -0700

    test cases for env_data, env_data_desc and mux

--- tests/test_diag_data_dictionary_spec.py
+++ tests/test_diag_data_dictionary_spec.py
@@ -3,6 +3,12 @@
 
 import unittest
 
+from odxtools.envdatadesc import EnvDataDesc
+from odxtools.mux import Mux, SwitchKey, DefaultCase, Case
+
+from odxtools import ValueParameter, PhysicalConstantParameter
+
+from odxtools.envdata import EnvData
 from odxtools.table import Table, TableRow
 
 from odxtools.compumethods import IdenticalCompuMethod
@@ -71,14 +77,84 @@ class TestDiagDataDictionarySpec(unittest.TestCase):
                 ]
             )
 
+        env_data = EnvData(
+            id="somersault.env_data.flip_env_data",
+            short_name="flip_env_data",
+            long_name="Flip Env Data",
+            parameters=[
+                ValueParameter(
+                    short_name="flip_speed",
+                    long_name="Flip Speed",
+                    byte_position=0,
+                    semantic="DATA",
+                    dop_ref="dop-ref",
+                ),
+                PhysicalConstantParameter(
+                    short_name="flip_direction",
+                    long_name="Flip Direction",
+                    byte_position=1,
+                    semantic="DATA",
+                    physical_constant_value=1,
+                    dop_ref="dop-ref",
+                ),
+            ]
+        )
+
+        env_data_desc = EnvDataDesc(
+            id="somersault.env_data_desc.flip_env_data_desc",
+            short_name="flip_env_data_desc",
+            long_name="Flip Env Data Desc",
+            param_snref="flip_speed",
+            env_data_refs=["somersault.env_data.flip_env_data"],
+        )
+
+        mux = Mux(
+            id="somersault.mux.flip_preference",
+            short_name="flip_preference",
+            long_name="Flip Preference",
+            byte_position=0,
+            switch_key=SwitchKey(
+                byte_position=0,
+                bit_position=0,
+                dop_ref="dop-ref",
+            ),
+            default_case=DefaultCase(
+                short_name="default_case",
+                long_name="Default Case",
+                structure_ref="structure_ref",
+            ),
+            cases=[
+                Case(
+                    short_name="forward_flip",
+                    long_name="Forward Flip",
+                    lower_limit="1",
+                    upper_limit="3",
+                    structure_ref="structure_ref",
+                ),
+                Case(
+                    short_name="backward_flip",
+                    long_name="Backward Flip",
+                    lower_limit="1",
+                    upper_limit="3",
+                    structure_ref="structure_ref",
+                )
+            ]
+        )
+
         ddds = DiagDataDictionarySpec(dtc_dops=[dtc_dop],
                                       data_object_props=[dop_1, dop_2],
-                                      tables=[table])
+                                      tables=[table],
+                                      env_data_descs=[env_data_desc],
+                                      env_datas=[env_data],
+                                      muxs=[mux])
 
         self.assertEqual(ddds.dtc_dops[0], dtc_dop)
         self.assertEqual(ddds.data_object_props[0], dop_1)
         self.assertEqual(ddds.data_object_props.another_dop, dop_2)
         self.assertEqual(ddds.tables[0], table)
+        self.assertEqual(ddds.env_data_descs[0], env_data_desc)
+        self.assertEqual(ddds.env_datas[0], env_data)
+        self.assertEqual(ddds.muxs[0], mux)
 
         self.assertEqual(len(ddds.structures), 0)
         self.assertEqual(len(ddds.end_of_pdu_fields), 0)
