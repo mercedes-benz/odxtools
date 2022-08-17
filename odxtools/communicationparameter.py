546b1e6ef93044d32a4556aa1751f9f022c5642a
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2022-08-17 17:39:52 +0200

    make VALUE the first if

--- odxtools/communicationparameter.py
+++ odxtools/communicationparameter.py
@@ -50,10 +50,10 @@ def read_communication_param_ref_from_odx(et_element):
     id_ref = et_element.get("ID-REF")
     # ODX standard v2.0.0 defined only VALUE
     # ODX standard v2.0.1 decided to break things and change it to choice between SIMPLE-VALUE and COMPLEX-VALUE
-    if et_element.find("SIMPLE-VALUE") is not None:
-        value = et_element.find("SIMPLE-VALUE").text
-    elif et_element.find("VALUE"):
-        value = et_element.find("VALUE").text
+    if et_element.find("VALUE") is not None:
+        value = et_element.findtext("VALUE")
+    elif et_element.find("SIMPLE-VALUE") is not None:
+        value = et_element.findtext("SIMPLE-VALUE")
     else:
         value = _read_complex_value_from_odx(et_element.find("COMPLEX-VALUE"))
 
