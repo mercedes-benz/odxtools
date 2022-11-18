c1ac3a3ac79e6b81c7e1ba82e6cdcbc7e3d9a631
Andreas Lauser <Andreas.Lauser@mbition.io>
2022-11-18 12:19:45 +0100

    writer cleanups
    - rename the jinja2 template files to *.jinja2.xml to make some IDEs happy
    - move the comparam fragments shipped with the ODX standard from the
      directory for templates to `examples/data`
    
    Signed-off-by: Andreas Lauser <andreas.lauser@mbition.io>
    Signed-off-by: Oliver Kopp <oliver.kopp@mbition.io>

--- odxtools/write_pdx_file.py
+++ odxtools/write_pdx_file.py
@@ -12,7 +12,7 @@ import inspect
 from typing import Any, Dict, List, Tuple
 
 from .exceptions import OdxError
-from .comparam_subset import BaseComparam
+from .comparam_subset import BaseComparam, Comparam, ComplexComparam
 
 odxdatabase = None
 
@@ -35,12 +35,12 @@ def comparam_subset_name(x: BaseComparam) -> str:
 
 __module_filename = inspect.getsourcefile(odxtools)
 assert isinstance(__module_filename, str)
-__pdx_stub_dir = os.path.sep.join([os.path.dirname(__module_filename),
-                                  "pdx_stub"])
+__templates_dir = os.path.sep.join([os.path.dirname(__module_filename),
+                                    "templates"])
 def write_pdx_file(output_file_name : str,
                    database : odxtools.Database,
                    auxiliary_content_specifiers : List[Tuple[str, bytes]] = [],
-                   stub_dir : str = __pdx_stub_dir) -> bool:
+                   templates_dir : str = __templates_dir) -> bool:
     """
     Write an internalized database to a PDX file.
     """
@@ -53,43 +53,43 @@ def write_pdx_file(output_file_name : str,
                          mode="w",
                          compression=zipfile.ZIP_DEFLATED) as zf:
 
-        # write all files in the stub directory
-        for root, dir, files in os.walk(stub_dir):
-            for stub_file_name in files:
+        # write all files in the templates directory
+        for root, dir, files in os.walk(templates_dir):
+            for template_file_name in files:
                 # we are not interested in the autosave garbage of
                 # editors...
-                if stub_file_name.startswith("."):
+                if template_file_name.startswith("."):
                     continue
-                elif stub_file_name.startswith("#"):
+                elif template_file_name.startswith("#"):
                     continue
-                elif stub_file_name.endswith("~"):
+                elif template_file_name.endswith("~"):
                     continue
-                elif stub_file_name.endswith(".bak"):
+                elif template_file_name.endswith(".bak"):
                     continue
-                elif stub_file_name.endswith(".tpl"):
+                elif template_file_name.endswith(".xml.jinja2"):
                     continue
-                elif stub_file_name.endswith(".odx-cs"):
+                elif template_file_name.endswith(".odx-cs"):
                     # we don't copy the comparam subset files (they
                     # are written based on the database)
                     continue
 
-                stub_file_mime_type = "text/plain"
-                if stub_file_name.endswith(".odx-cs"):
-                    stub_file_mime_type = "application/x-asam.odx.odx-cs"
-                elif stub_file_name.endswith(".odx-d"):
-                    stub_file_mime_type = "application/x-asam.odx.odx-d"
+                template_file_mime_type = "text/plain"
+                if template_file_name.endswith(".odx-cs"):
+                    template_file_mime_type = "application/x-asam.odx.odx-cs"
+                elif template_file_name.endswith(".odx-d"):
+                    template_file_mime_type = "application/x-asam.odx.odx-d"
 
                 in_path = list([root])
-                in_path.append(stub_file_name)
+                in_path.append(template_file_name)
                 in_file_name = os.path.sep.join(in_path)
 
-                stub_file_stats = os.stat(in_file_name)
-                stub_file_cdate = datetime.datetime.fromtimestamp(stub_file_stats.st_ctime)
-                stub_file_creation_date = stub_file_cdate.strftime("%Y-%m-%dT%H:%M:%S")
-                file_index.append( (stub_file_name,
-                                    stub_file_creation_date,
-                                    stub_file_mime_type) )
-                with zf.open(stub_file_name, "w") as out_file:
+                template_file_stats = os.stat(in_file_name)
+                template_file_cdate = datetime.datetime.fromtimestamp(template_file_stats.st_ctime)
+                template_file_creation_date = template_file_cdate.strftime("%Y-%m-%dT%H:%M:%S")
+                file_index.append( (template_file_name,
+                                    template_file_creation_date,
+                                    template_file_mime_type) )
+                with zf.open(template_file_name, "w") as out_file:
                     out_file.write(open(in_file_name, "rb").read())
 
         # write the auxiliary files
@@ -98,9 +98,9 @@ def write_pdx_file(output_file_name : str,
             creation_date = file_cdate.strftime("%Y-%m-%dT%H:%M:%S")
 
             mime_type = "text/plain"
-            if stub_file_name.endswith(".odx-cs"):
+            if template_file_name.endswith(".odx-cs"):
                 mime_type = "application/x-asam.odx.odx-cs"
-            elif stub_file_name.endswith(".odx-d"):
+            elif template_file_name.endswith(".odx-d"):
                 mime_type = "application/x-asam.odx.odx-d"
 
             zf_name = os.path.basename(output_file_name)
@@ -108,7 +108,7 @@ def write_pdx_file(output_file_name : str,
                 file_index.append( (zf_name, creation_date, mime_type) )
                 out_file.write(data)  # type: ignore
 
-        jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(stub_dir))
+        jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(templates_dir))
         jinja_env.globals['hasattr'] = hasattr
         jinja_env.globals['odxraise'] = jinja2_odxraise_helper
         jinja_env.globals['comparam_subset_name'] = comparam_subset_name
@@ -122,23 +122,32 @@ def write_pdx_file(output_file_name : str,
         vars["database"] = database
 
         # write the communication parameter subsets
-        comparam_subset_tpl = jinja_env.get_template("comparam-subset.odx-cs.tpl")
+        comparam_subset_tpl = jinja_env.get_template("comparam-subset.odx-cs.xml.jinja2")
         for comparam_subset in database.comparam_subsets:
             zf_file_name = f"{comparam_subset.short_name}.odx-cs"
             zf_file_cdate = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
             zf_mime_type = "application/x-asam.odx.odx-cs"
 
             vars["comparam_subset"] = comparam_subset
+            vars["simple_comparams"] = [x
+                                         for x in comparam_subset.comparams
+                                         if isinstance(x, Comparam)]
+            vars["complex_comparams"] = [x
+                                         for x in comparam_subset.comparams
+                                         if isinstance(x, ComplexComparam)]
 
             file_index.append( (zf_file_name,
                                 zf_file_cdate,
                                 zf_mime_type) )
 
             zf.writestr(zf_file_name, comparam_subset_tpl.render(**vars))
+
+        del vars["simple_comparams"]
+        del vars["complex_comparams"]
         del vars["comparam_subset"]
 
         # write the actual diagnostic data.
-        dlc_tpl = jinja_env.get_template("diag_layer_container.odx-d.tpl")
+        dlc_tpl = jinja_env.get_template("diag_layer_container.odx-d.xml.jinja2")
         for dlc in database.diag_layer_containers:
             vars["dlc"] = dlc
 
@@ -153,7 +162,7 @@ def write_pdx_file(output_file_name : str,
 
         # write the index.xml file
         vars["file_index"] = file_index
-        index_tpl = jinja_env.get_template("index.xml.tpl")
+        index_tpl = jinja_env.get_template("index.xml.xml.jinja2")
         text = index_tpl.render(**vars)
         zf.writestr("index.xml", text)
 
