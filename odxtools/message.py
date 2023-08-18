d638f12f95a20b1b9164a0cb979469c783f7e4f2
Andreas Lauser <andreas.lauser@mbition.io>
2023-08-18 15:30:36 +0200

    make `Message` a dataclass
    thanks to [at]kayoub5 for the nudges!
    
    Signed-off-by: Andreas Lauser <andreas.lauser@mbition.io>
    Signed-off-by: Katja Köhler <katja.koehler@mbition.io>

--- odxtools/message.py
+++ odxtools/message.py
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: MIT
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 from .odxtypes import ParameterValue, ParameterValueDict
@@ -8,6 +9,7 @@ if TYPE_CHECKING:
     from .structure import Structure
 
 
+@dataclass
 class Message:
     """A diagnostic message with its interpretation.
 
@@ -17,20 +19,10 @@ class Message:
     interpretation of the same data.
     """
 
-    def __init__(self, *, coded_message: bytes, service: "DiagService", structure: "Structure",
-                 param_dict: ParameterValueDict) -> None:
-        self.coded_message = coded_message
-        self.service = service
-        self.structure = structure
-        self.param_dict = param_dict
+    coded_message: bytes
+    service: "DiagService"
+    structure: "Structure"
+    param_dict: ParameterValueDict
 
     def __getitem__(self, key: str) -> ParameterValue:
         return self.param_dict[key]
-
-    def __str__(self) -> str:
-        param_string = ", ".join(
-            map(lambda param: f"{param[0]}={repr(param[1])}", self.param_dict.items()))
-        return f"{self.structure.short_name}({param_string})"
-
-    def __repr__(self) -> str:
-        return self.__str__()
