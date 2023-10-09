e0f80800c241997b2e071a6e1e39fb895ce4113a
Andreas Lauser <andreas.lauser@mbition.io>
2023-10-09 15:47:45 +0200

    load*_file(): add/fix type annotations
    also, some minor cleanups...
    
    Signed-off-by: Andreas Lauser <andreas.lauser@mbition.io>
    Signed-off-by: Gerrit Ecke <gerrit.ecke@mbition.io>

--- odxtools/load_odx_d_file.py
+++ odxtools/load_odx_d_file.py
@@ -1,9 +1,6 @@
 # SPDX-License-Identifier: MIT
 from .database import Database
-from .globals import logger
 
 
-def load_odx_d_file(odx_d_file_name: str):
-    container = Database(odx_d_file_name=odx_d_file_name)
-    logger.info(f"--- --- --- Done with parsing --- --- ---")
-    return container
+def load_odx_d_file(odx_d_file_name: str) -> Database:
+    return Database(odx_d_file_name=odx_d_file_name)
--- odxtools/load_pdx_file.py
+++ odxtools/load_pdx_file.py
@@ -2,11 +2,7 @@
 from zipfile import ZipFile
 
 from .database import Database
-from .globals import logger
 
 
-def load_pdx_file(pdx_file: str):
-    u = ZipFile(pdx_file)
-    container = Database(pdx_zip=u)
-    logger.info(f"--- --- --- Done with parsing --- --- ---")
-    return container
+def load_pdx_file(pdx_file: str) -> Database:
+    return Database(pdx_zip=ZipFile(pdx_file))
