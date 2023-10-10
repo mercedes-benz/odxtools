70d9c0acf2c7ca25a0727818901aa0c2147bbee2
Andreas Lauser <andreas.lauser@mbition.io>
2023-10-10 15:29:05 +0200

    _parser_utils.py: add/fix type annotations
    Signed-off-by: Andreas Lauser <andreas.lauser@mbition.io>
    Signed-off-by: Christian Hackenbeck <christian.hackenbeck@mbition.io>

--- odxtools/cli/_parser_utils.py
+++ odxtools/cli/_parser_utils.py
@@ -1,17 +1,20 @@
 #! /usr/bin/python3
 #
 # SPDX-License-Identifier: MIT
+import argparse
+
+from ..database import Database
 from ..load_file import load_file as _load_file
 
 
-def add_pdx_argument(parser):
-    parser.add_argument("pdx_file", nargs="?", metavar="PDX_FILE", help="path to the .pdx file")
-    # parser.add_argument("pdx_files", metavar="PDX_FILES", nargs="+", help="PDX descriptions of all ECUs which shall be analyzed")
+def add_pdx_argument(parser: argparse.ArgumentParser, is_optional: bool = False) -> None:
+    parser.add_argument(
+        "pdx_file",
+        help="Location of the .pdx file",
+        nargs="?" if is_optional else 1,
+        metavar="PDX_FILE")
 
 
-def load_file(args):
-    db_file_name = args.pdx_file
-    odxdb = None
-    if db_file_name is not None:
-        odxdb = _load_file(db_file_name)
-    return odxdb
+def load_file(args: argparse.Namespace) -> Database:
+    pdx_file_name = args.pdx_file if isinstance(args.pdx_file, str) else args.pdx_file[0]
+    return _load_file(pdx_file_name)
