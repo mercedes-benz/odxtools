a8edd4bfb9fede1770925ab8cc2c5361e874eebe
Andreas Lauser <andreas.lauser@mercedes-benz.com>
2025-02-19 15:30:18 +0100

    EncodeState: fix another use-before assignment error
    as the previous commit, this only affects the non-strict mode. (This
    time I also checked that there are no more instances of this and that
    the the reverse does not happen in the inverse function of `DecodeState`...
    
    again, thanks to [at]nada-ben-ali for finding this!
    
    Signed-off-by: Andreas Lauser <andreas.lauser@mercedes-benz.com>

--- odxtools/encodestate.py
+++ odxtools/encodestate.py
@@ -157,7 +157,7 @@ class EncodeState:
                 odxraise(f"Illegal encoding ({base_type_encoding}) specified for "
                          f"{base_data_type.value}")
 
-                internal_value = raw_value
+                raw_value = internal_value
 
         # ... unsigned integers, ...
         elif base_data_type == DataType.A_UINT32:
