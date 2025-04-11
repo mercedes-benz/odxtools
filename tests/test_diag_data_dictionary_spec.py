1ef4e45d423b5bc71a5042608b0eb25ec1b2a223
nada <nada.benali@technica-engineering.de>
2025-04-11 10:04:35 +0100

    fix coding style

--- tests/test_diag_data_dictionary_spec.py
+++ tests/test_diag_data_dictionary_spec.py
@@ -280,8 +280,9 @@ class TestDiagDataDictionarySpec(unittest.TestCase):
         if mux.default_case:
             mux.default_case.structure_ref = OdxLinkRef.from_id(mux_case2_struct.odx_id)
         ecu._resolve_odxlinks(odxlinks)
-        self.assertEqual(mux.get_static_bit_length(),  # mux cases doesn't have the same structure size
-                         None)
+        self.assertEqual(  # mux cases doesn't have the same structure size
+            mux.get_static_bit_length(),
+            None)
         mux.cases[0].structure_ref = OdxLinkRef.from_id(mux_case2_struct.odx_id)
         ecu._resolve_odxlinks(odxlinks)
         self.assertEqual(mux.get_static_bit_length(), 16)
